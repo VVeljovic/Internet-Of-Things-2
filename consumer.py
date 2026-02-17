528994bb83443d85a4d8448f1a97a679915667f9
VVeljovic <veljkoveljovic13@gmail.com>
2026-02-17 13:25:53 +0100

    in

--- consumer.py
+++ consumer.py
@@ -2,6 +2,25 @@ import paho.mqtt.client as paho
 import re
 from influxdb import InfluxDBClient
 
+def add_to_buffer (x,y,z,orientation):
+	data = {
+		"measurement" : "imu_data",
+		"tags" : {"orientation" : orientation},
+		"fields" : {
+		"x": x,
+		"y": y,
+		"z": z},
+		"time" : datetime.utcnow().isoformat()
+		}
+	if orientation == "VERTICAL":
+		vertical_array.append(data)
+		if len(vertical_array) == BATCH_SIZE:
+			influxClient.write_points(vertical_array)
+			print("vertical buffer stored")
+			
+	if orientation == "HORIZONTAL":
+		horizontal_array.append(data)
+
 def on_connect(client,userdata,flags,rc):
 	print("Connected with code:", rc)
 	client.subscribe("sensors/nano33")
@@ -25,6 +44,7 @@ def on_message(client, userdata, msg):
 				"tags" : { "location": "arduino_nano_33_ble_sense_lite", "orientation" : label},
 				"fields" : {"x":x, "y":y, "z":z}
 			}]
+		
 		influxClient.write_points(data)
 
 influxClient = InfluxDBClient(
@@ -37,7 +57,9 @@ influxClient = InfluxDBClient(
 if {'name': 'iotdb'} not in influxClient.get_list_database():
 	influxClient.create_database('iotdb')
 
-
+horizontal_array = []
+vertical_array = []
+diagonal_array = []
 client = paho.Client()
 client.on_connect = on_connect
 client.on_message = on_message
