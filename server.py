bd1d441c866b71162bb84068ee206e7ab2cc1d49
veljk <veljk@VELJKO>
2026-03-11 11:55:46 +0100

    server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from influxdb import InfluxDBClient
 import paho.mqtt.client as mqtt
+
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +16,28 @@ influxClient = InfluxDBClient(
 	password='userpass',
 	database='iotdb'
 )
+
+def on_connect(client,userdata,flags,rc):
+	print("Connected with code:", rc)
+	client.subscribe("notification")
+
+@app.route("/set-threshold", methods=["POST"])
+def set_threshold():
+	data = request.get_json()
+	threshold = data.get("threshold")
+	if threshold is not None:
+		mqttClient.publish("configuration", str(threshold))
+		print("Poslat threshold:", threshold)
+		return jsonify({"status": "ok", "threshold": threshold})
+	return jsonify({"status": "error", "message": "Threshold nije prosleđen"}), 400
+
+@app.route("/set-anomaly", methods=['POST'])
+def set_anomaly():
+	data = request.get_json()
+	anomaly = data.get("anomaly")
+	mqttClient.publish("anomaly", anomaly)
+	return jsonify({"status": "ok", "anomaly": anomaly})
+
 
 @app.route('/simulate')
 def simulate():
@@ -37,12 +60,13 @@ def handle_disconnect():
 
 def on_mqtt_message(client, userdata, msg):
 	payload = msg.payload.decode()
+	print("MQTT poruka:", payload)
 	socketio.emit("new-data", {"data": payload})
 
 mqttClient = mqtt.Client()
 mqttClient.on_message = on_mqtt_message
+mqttClient.on_connect = on_connect
 mqttClient.connect("localhost", 1883, 60)
-mqttClient.subscribe("sensors/nano33")
 mqttClient.loop_start()
 
 
